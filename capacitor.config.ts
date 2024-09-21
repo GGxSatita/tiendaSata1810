28f87f595b0d0361164f1dd0ca3f9bafc21a35a4
Iván Carreño <105950912+GGxSatita@users.noreply.github.com>
2024-09-21 11:17:13 -0300

    inicio de sata tienda

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'tiendaSata1810',
+  appId: 'app.web.tiendaSata',
+  appName: 'Tienda Sata',
   webDir: 'www'
 };
 
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,5 +9,6 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
+
   constructor() {}
 }
